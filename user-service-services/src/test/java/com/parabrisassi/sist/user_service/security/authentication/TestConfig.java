94cd2ab2025178145dfb52e13676932a7f5c8029
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-01-17 21:28:33 -0300

    Add testing for the PasswordValidatorImpl class

---
+++ user-service-services/src/test/java/com/parabrisassi/sist/user_service/security/authentication/TestConfig.java
@@ -0,0 +1,13 @@
+package com.parabrisassi.sist.user_service.security.authentication;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Juan Marcos Bellini on 17/1/18.
+ * Questions at jbellini@bellotapps.com
+ */
+@Configuration
+@ComponentScan(value = "com.parabrisassi.sist.user_service.security.authentication", lazyInit = true)
+public class TestConfig {
+}
